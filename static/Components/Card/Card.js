abd7cb5afc7cac418e693643dd43b7820c81293c
giosofranco <100816931+giosofranco@users.noreply.github.com>
2022-10-09 17:32:43 +0100

    Add files via upload

---
+++ static/Components/Card/Card.js
@@ -0,0 +1,15 @@
+import './Card.scss'
+
+const Card = props => {
+    return ( <
+        div className = {
+            `card_style ${props?.className}`
+        } > {
+            props.children
+        } <
+        /div>
+    )
+
+}
+
+export default Card;
